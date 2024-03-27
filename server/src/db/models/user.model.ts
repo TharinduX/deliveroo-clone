2eb9b3302e2f639daa99ebdd04883a7863aa3848
Tharindu Jayasanka <hello@tharindu.me>
2024-03-27 15:30:18 +0530

    feat: user register

---
+++ server/src/db/models/user.model.ts
@@ -0,0 +1,77 @@
+import {
+  Table,
+  Column,
+  Model,
+  AllowNull,
+  Unique,
+  DataType,
+  CreatedAt,
+  UpdatedAt,
+} from 'sequelize-typescript';
+
+enum Role {
+  OWNER = 'owner',
+  CUSTOMER = 'customer',
+}
+
+@Table({
+  timestamps: true,
+  tableName: 'users',
+  modelName: 'User',
+})
+class User extends Model {
+  @Column({
+    primaryKey: true,
+    type: DataType.UUID,
+    defaultValue: DataType.UUIDV4,
+  })
+  declare id: string;
+
+  @Unique
+  @AllowNull(false)
+  @Column({
+    type: DataType.STRING,
+  })
+  declare email: string;
+
+  @AllowNull(false)
+  @Column({
+    type: DataType.STRING,
+  })
+  declare passwordHash: string;
+
+  @AllowNull(false)
+  @Column({
+    type: DataType.ENUM(...Object.values(Role)),
+  })
+  declare role: Role;
+
+  @AllowNull(false)
+  @Column({
+    type: DataType.STRING,
+  })
+  declare phone: string;
+
+  @Column({
+    type: DataType.STRING,
+  })
+  declare firstName: string;
+
+  @Column({
+    type: DataType.STRING,
+  })
+  declare lastName: string;
+
+  @Column({
+    type: DataType.STRING,
+  })
+  declare refreshToken: string;
+
+  @CreatedAt
+  declare createdAt: Date;
+
+  @UpdatedAt
+  declare updatedAt: Date;
+}
+
+export default User;
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,24 @@
-import express, { Express, Request, Response } from 'express';
-const port = 8000;
+import express, { Express } from 'express';
+import userRoutes from '@routes/user.routes.js';
+import sequelize from './db/config.js';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const port = process.env.PORT!;
 const app: Express = express();
+app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!!!!');
-});
+//User routes
+app.use('/api/user', userRoutes);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+sequelize
+  .sync()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the database:', error);
+  });
